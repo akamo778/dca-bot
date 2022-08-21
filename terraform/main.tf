39d3456378208257cbe208bae63f6b899a939a9f
Ryo Hashimoto <ryo.hashimoto.2525@gmail.com>
2022-08-21 18:45:43 +0900

    add cloud scheduler

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,8 +36,49 @@ resource "google_cloud_run_service" "default" {
     latest_revision = true
   }
 
-  # Use an explicit depends_on clause to wait until API is enabled
   depends_on = [
     google_project_service.run_api
   ]
+}
+
+resource "google_service_account" "default" {
+  account_id   = "mydcabot-scheduler-sa"
+  description  = "Cloud Scheduler service account; used to trigger scheduled Cloud Run jobs."
+  display_name = "mydcabot-scheduler-sa"
+
+  depends_on = [
+    google_project_service.iam_api
+  ]
+}
+
+resource "google_cloud_scheduler_job" "default" {
+  name             = "mydcabot-scheduled-cloud-run-job"
+  description      = "Invoke a mydcabot Cloud Run container on a schedule."
+  schedule         = "0 */6 * * *"
+  time_zone        = "Asia/Tokyo"
+  attempt_deadline = "320s"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = google_cloud_run_service.default.status[0].url
+
+    oidc_token {
+      service_account_email = google_service_account.default.email
+    }
+  }
+
+  depends_on = [
+    google_project_service.scheduler_api
+  ]
+}
+
+resource "google_cloud_run_service_iam_member" "default" {
+  location = google_cloud_run_service.default.location
+  service = google_cloud_run_service.default.name
+  role = "roles/run.invoker"
+  member = "serviceAccount:${google_service_account.default.email}"
 }
