0fee60faa61deeb2578d430a55ceb5d534d8dc89
Ryo Hashimoto <ryo.hashimoto.2525@gmail.com>
2022-08-21 13:01:04 +0900

    init terraform

---
+++ terraform/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.2.7"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.32.0"
+    }
+  }
+  cloud {
+    organization = "ryohashimoto"
+
+    workspaces {
+      name = "dca-bot"
+    }
+  }
+}
