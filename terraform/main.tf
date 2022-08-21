e7cef3d86ad33d658e95ec0f6d07324269409a5a
Ryo Hashimoto <ryo.hashimoto.2525@gmail.com>
2022-08-21 18:06:48 +0900

    add cloud_run service

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,27 @@ resource "google_project_service" "scheduler_api" {
   service            = "cloudscheduler.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_cloud_run_service" "default" {
+  project  = var.project_id
+  name     = "mydcabot_service"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  # Use an explicit depends_on clause to wait until API is enabled
+  depends_on = [
+    google_project_service.run_api
+  ]
+}
