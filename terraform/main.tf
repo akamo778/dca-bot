598202c18c04fbdf6cb2f4af70cbcec996fbeeb4
Ryo Hashimoto <ryo.hashimoto.2525@gmail.com>
2022-08-21 18:16:29 +0900

    minor refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,28 +3,18 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_project_service" "run_api" {
-  project                    = var.project_id
-  service                    = "run.googleapis.com"
-  disable_dependent_services = true
-  disable_on_destroy         = false
+locals {
+  services = toset([
+    "run.googleapis.com",
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "cloudscheduler.googleapis.com"
+  ])
 }
 
-resource "google_project_service" "iam_api" {
-  project            = var.project_id
-  service            = "iam.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "resource_manager_api" {
-  project            = var.project_id
-  service            = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "scheduler_api" {
-  project            = var.project_id
-  service            = "cloudscheduler.googleapis.com"
+resource "google_project_service" "service" {
+  for_each           = local.services
+  service            = each.value
   disable_on_destroy = false
 }
 
